fdb944c7d67f49864c96cf635633ebaab2ec9f6a
Alfi Yusrina <alfi.yusrina@hotmail.com>
2017-03-25 14:54:02 +0100

    :tada: initial commit

---
+++ src/js/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <header>
+      <h1>Hello, tribute</h1>
+    </header>
+  );
+};
+
+export default App;
---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+import {render} from 'react-dom';
+import App from './containers/App';
+
+const init = () => {
+
+  render(
+    <App />,
+    document.querySelector(`.react-mount`)
+  );
+
+};
+
+init();
