5082da62d5ad89ba27e590314bf877cf94d76319
Alfi Yusrina <alfi.yusrina@hotmail.com>
2017-03-25 23:03:58 +0100

    background image mustache added

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div>
     <section className='cats'>
       <header>
-        <h1>Freddie is ailurophile</h1>
+        <h1 className='section-title'>Freddie is ailurophile</h1>
         <p>Or in other words, is a cat lover. One of the most important things in the life of Freddie Mercury were his cats. He thought of them as equally important as humans. He had a lot of cats in his life, at a certain point he even had 10!
         </p>
       </header>
@@ -21,29 +21,29 @@ const App = () => {
       </article>
     </section>
     <section className='spoiled-cats'>
-      <header>
-        <h1>Spoiled Cats</h1>
+      <header >
+        <h1 className='section-title'>Spoiled Cats</h1>
       </header>
     </section>
     <section className='what-now'>
       <header>
-        <h1>What Now</h1>
+        <h1 className='section-title'>What Now</h1>
       </header>
     </section>
     <section className='foods'>
       <header>
-        <h1>Light Eater</h1>
+        <h1 className='section-title'>Light Eater</h1>
       </header>
     </section>
     <section className='wine'>
       <header>
-        <h1>Wine Lover</h1>
+        <h1 className='section-title'>Wine Lover</h1>
       </header>
       <p>Based on his personal assistant, as a rough guide Freddie drinks tea in the morning, the tea was Twinings Earl Grey. He likes tea with sugars and some milk, like other Brits do. In the later afternoon, he drinks champagne or wine and with food. The champagne was usually Louis Roederer Cristal and the white wine was his favourite imported from Switzerland, San Saphorin. Freddie was actually quite knowledgeable about wine and he would know which one he would want with various meals with friends. In the evening, he drinks vodka and tonic. His favourite vodka was Stolichnaya, but in some countries that isn’t available so he would drink Moscovskaya, really the same under a different name. Schweppes was the preferred mixer.</p>
     </section>
     <section className='fav-restos'>
       <header>
-        <h1>Favourite Restaurants</h1>
+        <h1 className='section-title'>Favourite Restaurants</h1>
         <p>Over the years Freddie had many favourite restaurants and would spend many happy hours in them with friends. It is a shame most of them don’t exist anymore. To know more about his favourite restaurants, click on one of the icons.</p>
       </header>
       <article>
@@ -55,13 +55,13 @@ const App = () => {
     </section>
     <section className='hot-spicy'>
       <header>
-        <h1>Hot Why Not</h1>
+        <h1 className='section-title'>Hot Why Not</h1>
       </header>
       <p>Freddie loved spicy food, not necessarily chilli hot, but a good mix of spice. This was natural if you think back to his birthplace, Zanzibar, the spice island, and his mother would have prepared many traditional Persian dishes for him. He did like traditional ‘home cooking’, as well as the five stars gourmet restaurants. He always used to say at home he wanted mother’s cooking. For deluxe food there were plenty of restaurants to choose from. As his illness progressed so his taste buds suffered and he couldn’t take strong flavours. At that time his personal chef started cooking food with much less spice and made it easy to eat food. A perfect example of that progression was with scrambled eggs. At the start the chef always cooked the Indian variety with chilli and spices, that Freddie remembered from his youth, but by the last year it was served with only a small sprinkling of salt and pepper.</p>
     </section>
     <section className='xmas-dining'>
       <header>
-        <h1>Christmas Dining with Freddie</h1>
+        <h1 className='section-title'>Christmas Dining with Freddie</h1>
         <p>Usually either on Christmas Eve or the day before, Freddie would invite friends around in the early evening for mince pies, sandwiches, and carol singing around the piano. Freddie was a great believer in tradition.</p>
       </header>
       <article>
@@ -73,7 +73,7 @@ const App = () => {
     </section>
     <section className='japan'>
       <header>
-        <h1>Sushi and Sashimi</h1>
+        <h1 className='section-title'>Sushi and Sashimi</h1>
       </header>
       <p>After touring Japan with Queen in 1975, Freddie’s favourite place on earth was Japan, its culture and art. He was overwhelmed by the reception Japanese audiences. If Japan is well known for their tasty traditional tasty foods, such as sushi and sashimi. Well, it’s not the first thing what Freddie loves about Japan. Life could be so frantic and Tokyo was always so busy, but he continually found it a place to recharge his batteries. He loves Japanese arts. He had a lovely collection of lacquered boxes, furniture in Japanese motifs, Japanese crockery and porcelains, and Hakata dolls.</p>
       <p>He was acknowledged once as one of  persons who has the largest collections of antique Japanese wood block prints in a private collection in the UK.</p>
