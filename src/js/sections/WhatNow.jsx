066cb32476987d407eaa0874fe839243cc5228b7
Alfi Yusrina <alfi.yusrina@hotmail.com>
2017-03-27 00:47:48 +0200

    call delilah works perfect

--- src/js/sections/WhatNow.jsx
+++ src/js/sections/WhatNow.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const WhatNow = () => {
   return (
     <section className='what-now'>
-      <header>
-        <h1 className='section-title'>What Now</h1>
-      </header>
-      <p className='section-text'>Freddie Mercury knew he was going to die, but before that happened, he made sure that all of his beloved cats would be taken care of. All of them stayed at Garden Lodge and Mary took care of them.  Today, they still live there and Delilah is often spotted on the top of the wall. So it’s clear that she is still the ruler of the house and that’s exactly what Freddie would have wanted.
-      </p>
+      <div className='what-now-insider'>
+          <header>
+            <h1 className='section-title'>What Now</h1>
+          </header>
+          <p className='section-text'>Freddie Mercury knew he was going to die, but before that happened, he made sure that <span className='what-now-highlight'>all of his beloved cats would be taken care of</span>. All of them stayed at Garden Lodge and Mary took care of them.  Today, they still live there and Delilah is often spotted on the top of the wall. So it’s clear that she is still the ruler of the house and that’s exactly what Freddie would have wanted.
+          </p>
+          <img className='what-now-img' src='assets/img/catsleepy.gif' alt='Cat is asleep' title='Sleepy Cat' />
+      </div>
     </section>
   );
 };
--- src/js/sections/WineLover.jsx
+++ src/js/sections/WineLover.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import DrinkWidget from '../components/DrinkWidget';
 const WineLover = () => {
   return (
     <section className='wine'>
-      <header>
-        <h1 className='section-title'>Wine Lover</h1>
-      </header>
-      <p className='section-text'>Based on his personal assistant, as a rough guide Freddie drinks tea in the morning, the tea was Twinings Earl Grey. He likes tea with sugars and some milk, like other Brits do. In the later afternoon, he drinks champagne or wine and with food. The champagne was usually Louis Roederer Cristal and the white wine was his favourite imported from Switzerland, San Saphorin. Freddie was actually quite knowledgeable about wine and he would know which one he would want with various meals with friends. In the evening, he drinks vodka and tonic. His favourite vodka was Stolichnaya, but in some countries that isn’t available so he would drink Moscovskaya, really the same under a different name. Schweppes was the preferred mixer.</p>
+      <div className='wine-insider flex'>
+        <div className='wine-texts'>
+        <header>
+          <h1 className='section-title'>Wine Lover</h1>
+        </header>
+        <p className='section-text'>Based on his personal assistant, as a rough guide Freddie drinks <span className='wine-highlight'>tea in the morning,</span> the tea was Twinings Earl Grey. He likes tea with sugars and some milk, like other Brits do. <span className='wine-highlight'>In the later afternoon</span>, he drinks <span className='wine-highlight'>champagne or wine</span> and with food. The champagne was usually Louis Roederer Cristal and the white wine was his favourite imported from Switzerland, San Saphorin. Freddie was actually quite knowledgeable about wine and he would know which one he would want with various meals with friends. <span className='wine'>In the evening,</span> he drinks <span className='wine-highlight'>vodka and tonic.</span> His favourite vodka was Stolichnaya, but in some countries that isn’t available so he would drink Moscovskaya, really the same under a different name. Schweppes was the preferred mixer.</p>
+        </div>
+        <div className='wine-images'>
+          <img  className='wine-img' src='assets/img/freddiedrinks.png' />
+          <DrinkWidget />
+        </div>
+      </div>
     </section>
   );
 };
