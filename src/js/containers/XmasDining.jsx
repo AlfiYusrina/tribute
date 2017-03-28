a59c5914d7c5619968d749270ff54506110c65cf
Alfi Yusrina <alfi.yusrina@hotmail.com>
2017-03-28 21:55:19 +0200

    am to pm changed

--- src/js/containers/XmasDining.jsx
+++ src/js/containers/XmasDining.jsx
@@ -10,19 +10,19 @@ const data =  {
     text: `Christmas Day dawned and Freddie had a small breakfast at about 9 am. Freddie would be going round the house making sure everything was in place for his guests. You would hear his laughter louder than the Christmas carols coming out of the sound system.`
   },
   '1PM': {
-    time: `1AM`,
+    time: `1PM`,
     img: `assets/img/teatime.png`,
     title: `CHAMPANGE TIME`,
     text: `Everyone was always prompt and the house was full of voices by 1pm. Champagne was opened and served (opened by Jim and served by others) and Freddie was everywhere.`
   },
   '3PM': {
-    time: `3AM`,
+    time: `3PM`,
     img: `assets/img/lunch.png`,
     title: `LUNCH TIME`,
     text: `Lunch was served at 2 pm. After that they were opening their gifts.  The Queen’s Speech was on the TV at 3 pm. Generally they were enjoying tea/coffee and the cakes. The house was eerily quiet for a few hours as the weight of the food in stomachs brought down the eyelids. `
   },
   '8PM': {
-    time: `5AM`,
+    time: `8PM`,
     img: `assets/img/tv.png`,
     title: `TV TIME`,
     text: `By 8 pm, they were busy making turkey and stuffing sandwiches, heating up plates of other food and generally pigging out to spend a few hours in front of the TV before a well deserved rest, to get ready for what Boxing Day would bring.`
